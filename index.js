86437e33e7b02a301b3fa00a90c05be5e0a090ea
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2024-04-05 14:57:34 -0300

    remove comments

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ const promClient = require('prom-client');
 const app = express();
 const port = 3000;
 
-// Colete as métricas padrão do Node.js
+
 promClient.collectDefaultMetrics();
 
-// Métrica personalizada para contar o número de vezes que a rota '/' é acessada
 const contadorRequisicoes = new promClient.Counter({
   name: 'minhas_requisicoes_total',
   help: 'Número total de requisições na rota /',
 });
   
 app.get('/', (req, res) => {
-  // Incrementa a métrica personalizada
   contadorRequisicoes.inc();
   res.send('prometheus rodando!');
 });
 
-// Exponha as métricas para o Prometheus
 app.get('/metrics', async (req, res) => {
     try {
       res.set('Content-Type', promClient.register.contentType);
